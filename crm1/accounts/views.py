0f661eb4d4f8404f7c0fb0ae79af662e301e0581
alijamil22 <alijamil2223344@gmail.com>
2026-03-05 15:35:43 +0500

    Done 3 parts of denis Ivy CRM crash course project.Setup project and folders,Build templates and write some urls and views.

---
+++ crm1/accounts/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+# Create your views here.
+def home(request):
+    return render(request,'accounts/dashboard.html')
+def products(request):
+    return render(request,'accounts/products.html')
+def customer(request):
+    return render(request,'accounts/customers.html')
