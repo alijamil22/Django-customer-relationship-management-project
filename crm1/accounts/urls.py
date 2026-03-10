5d0dab9e8df9f8453aa71922db8691c5bc98e76c
alijamil22 <alijamil2223344@gmail.com>
2026-03-10 22:16:41 +0500

    done Dynamic url part 1. Make dynamic customer template.

--- crm1/accounts/urls.py
+++ crm1/accounts/urls.py
@@ -3,5 +3,5 @@ from . import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('products/',views.products,name='products'),
-    path('customers/',views.customer,name='customer'),
+    path('customers/<str:pk>/',views.customer,name='customer'),
 ]
--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 # Create your views here.
 def home(request):
-    return render(request,'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='pending').count()
+    
+    return render(request,'accounts/dashboard.html', {'orders': orders, 'customers': customers, 'total_customers': total_customers, 'total_orders': total_orders, 'delivered': delivered, 'pending': pending})
 def products(request):
-    return render(request,'accounts/products.html')
-def customer(request):
-    return render(request,'accounts/customers.html')
+    products = Product.objects.all()
+    return render(request,'accounts/products.html', {'products': products})
+def customer(request,pk):
+    customer = Customer.objects.get(id=pk)
+    order = customer.order_set.all()
+    orders_count = order.count()
+    context = {'customer': customer, 'order': order, 'orders_count': orders_count}
+    return render(request,'accounts/customers.html', context)
